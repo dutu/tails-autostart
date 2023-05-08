d0fc19f2e72b5509b3690fede06e8f46b66a4caa
dutu <dutu@protonmail.com>
2023-05-08 21:22:15 +0300

    fix `install_dir`

--- src/tails-autostart/startup_mods.sh
+++ src/tails-autostart/startup_mods.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+persistence_dir=/live/persistence/TailsData_unlocked
+install_dir=${persistence_dir}/tails-autostart
+persistence_dir=/live/persistence/TailsData_unlocked
+
 # Run root changes if root.d contains any *.sh files
 if ls ${HOME}/.config/autostart/root.d/*.sh 1> /dev/null 2>&1
 then
-  pkexec bash -x ${HOME}/.config/autostart/run_dir_scripts.sh root
+  pkexec bash -x ${install_dir}/run_dir_scripts.sh root
 fi
 # Run non-root changes
-bash -x ${HOME}/.config/autostart/run_dir_scripts.sh amnesia
+bash -x ${install_dir}/run_dir_scripts.sh amnesia
