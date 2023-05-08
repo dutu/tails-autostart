88232476a1809924d37f0f12123ecde9ebf5b898
dutu <dutu@protonmail.com>
2023-05-08 09:55:21 +0300

    suppress `ls` output

--- src/install_tails_autostart.sh
+++ src/install_tails_autostart.sh
@@ -10,18 +10,18 @@ then
     exit 1
 fi
 
-echo "Moving files to autostart directory '${autostart_dir}'..."
+echo "Copying files to autostart directory '${autostart_dir}'..."
 mkdir -p ${autostart_dir}
 rsync -av --stats --delete autostart/ ${autostart_dir}/ > /dev/null
 
 # Check if ${install_dir} contains any files and delete them if it does
-if [ "$(ls -A "${install_dir}")" ]
+if [ "$(ls -A "${install_dir}" 2>/dev/null)" ]
 then
   echo "Files found in installation directory '${install_dir}'. Deleting them..."
-  rm "${install_dir}/*"
+  rm -rf "${install_dir}"
 fi
 
-echo "Moving files to installation directory '${install_dir}'..."
+echo "Copying files to installation directory '${install_dir}'..."
 mkdir -p ${install_dir}
 rsync -a --stats --delete tails-autostart/ ${install_dir}/ > /dev/null
 rsync -a --stats --delete version-* ${install_dir}/ > /dev/null
